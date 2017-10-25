d26ffc193e72cf5c5e5f641421bb729ce004fb61
gose <gom0367@gmail.com>
2017-10-25 20:06:08 +0900

    React simple 파일 생성 및 컴포넌트 생성

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,12 @@
+// 새 컴포넌트. 
+// Create a new component. This component should produce
+// some HTML
+const App = function() {
+  return <div>안뇽!</div>;
+  // const : ES6 표현 중 하나 이전에는 변수 선언에 var 를 사용 -> const로 선언한 변수는 절대로 변하지 않는 상수. 재할당 되지 않는다.
+  // JSX : 부분적인 템플릿 혹은 변형된 자바스크립트 표면적으로 보이지 않는 자바스크립트
+}
+
+// 리액트야 내가 만든 컴포넌트를 html로 넣어줘
+// Take This Components generated HTML and Put it
+// on the page (in the DOM)
